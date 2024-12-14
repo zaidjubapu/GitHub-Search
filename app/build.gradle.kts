47bcbcefc0021dd8b4dbbd5de4c1a4e6e0db0550
Zaid <zaid@zkteco.in>
2024-12-14 20:07:48 +0530

    code clean

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
     implementation(libs.hilt)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
     kapt(libs.hilt.compiler)
     implementation(libs.coroutines.android)
--- app/src/main/java/com/zkteco/gitsearchhub/di/AppModule.kt
+++ app/src/main/java/com/zkteco/gitsearchhub/di/AppModule.kt
@@ -1,6 +1,5 @@
 package com.zkteco.gitsearchhub.di
 
-import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
@@ -17,20 +16,12 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -48,8 +39,6 @@ class AppModule {
         .add(KotlinJsonAdapterFactory())
         .build()
 
-    @Provides
-    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
     @Singleton
     @Provides
@@ -57,42 +46,7 @@ class AppModule {
         @ApplicationContext context: Context
     ): OkHttpClient {
 
-        val trustAllCerts = arrayOf<TrustManager>(@SuppressLint("CustomX509TrustManager")
-        object : X509TrustManager {
-            @SuppressLint("TrustAllX509TrustManager")
-            override fun checkClientTrusted(
-                chain: Array<out X509Certificate>?,
-                authType: String?
-            ) {
-            }
-
-            @SuppressLint("TrustAllX509TrustManager")
-            override fun checkServerTrusted(
-                chain: Array<out X509Certificate>?,
-                authType: String?
-            ) {
-            }
-
-            override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
-        })
-        // Install the all-trusting trust manager
-        val sslContext = SSLContext.getInstance("SSL")
-        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-        // Create an ssl socket factory with our all-trusting manager
-        val sslSocketFactory = sslContext.socketFactory
-
-
-        val interceptor = Interceptor { chain ->
-            val original = chain.request()
-            val requestBuilder = original.newBuilder()
-
-            chain.proceed(requestBuilder.build())
-
-
-        }
-        return OkHttpClient.Builder().addInterceptor(interceptor)
-                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-                .hostnameVerifier { _, _ -> true }
+        return OkHttpClient.Builder()
                 .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                 .connectTimeout(ApiHelper.REQUEST_TIMEOUT_DURATION.toLong(), TimeUnit.SECONDS)
                 .readTimeout(ApiHelper.REQUEST_TIMEOUT_DURATION.toLong(), TimeUnit.SECONDS)
